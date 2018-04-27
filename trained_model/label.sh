17c7aa144e9eef7d681510b656af190c690c0877
Cameron Moberg <cjmoberg@gmail.com>
2018-04-27 12:07:09 -0500

    Added instructions

--- trained_model/label.sh
+++ trained_model/label.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-IMAGE=$1
+#Call the program like `./label.sh <PATH_TO_IMAGE>
 
+#Overall on a random sample 83% correct rate.
+
+IMAGE=$1
 python label_image.py --labels=retrained_labels.txt --graph=retrained_graph.pb --image=${IMAGE};
